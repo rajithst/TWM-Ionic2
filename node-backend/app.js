e7c6a84d48d1b4a2fef96d39d8b4a382399171ae
rajithst <rajiththennakoonshady@gmail.com>
2017-04-15 18:10:56 +0530

    add mongo connection and basic setup

--- node-backend/app.js
+++ node-backend/app.js
@@ -1,12 +1,27 @@
 const express =require('express');
 const  bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+const conf = require('./conf/db');
+
+var connection = mongoose.connect(conf.db);
+
+if(connection){
+    console.log("connected")
+}else {
+    console.log("error in mongodb")
+}
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
+
+
 
 
 app.listen(port,()=>{
