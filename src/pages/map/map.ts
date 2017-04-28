ad267b44b7acc7f4611e65a02af9a91e0c9bf832
Madura Herath <maduraherath8@gmail.com>
2017-04-29 01:45:14 +0530

    add geolocation

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import {
+ GoogleMaps,
+ GoogleMap,
+ GoogleMapsEvent,
+ LatLng,
+ CameraPosition,
+ MarkerOptions,
+ Marker
+} from '@ionic-native/google-maps';
 /**
  * Generated class for the Map page.
  *
@@ -16,7 +24,42 @@ export class Map {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
+ngAfterViewInit() {
+ this.loadMap();
+}
+loadMap() {
+  let element: HTMLElement = document.getElementById('map');
 
+ let map: GoogleMap = this.googleMaps.create(element);
+
+ // listen to MAP_READY event
+ // You must wait for this event to fire before adding something to the map or modifying it in anyway
+ map.one(GoogleMapsEvent.MAP_READY).then(() => console.log('Map is ready!'));
+
+ // create LatLng object
+ let ionic: LatLng = new LatLng(43.0741904,-89.3809802);
+
+ // create CameraPosition
+ let position: CameraPosition = {
+   target: ionic,
+   zoom: 18,
+   tilt: 30
+ };
+
+ // move the map's camera to position
+ map.moveCamera(position);
+
+ // create new marker
+ let markerOptions: MarkerOptions = {
+   position: ionic,
+   title: 'Ionic'
+ };
+
+ const marker: Marker = map.addMarker(markerOptions)
+   .then((marker: Marker) => {
+      marker.showInfoWindow();
+    });
+}
   ionViewDidLoad() {
     console.log('ionViewDidLoad Map');
   }
