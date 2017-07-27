b125a8ee4bc17e2ba7a3b378b5b36ee1c5266e2c
Madura Herath <maduraherath8@gmail.com>
2017-07-27 18:50:22 +0530

    add articlles

--- config/database.js
+++ config/database.js
@@ -5,5 +5,6 @@
 module.exports = {
 
     database:"mongodb://127.0.0.1:27017/travelbook",
+    // database:"mongodb://user:user123@ds035036.mlab.com:35036/travelwithme",
     secret:"travelbooksecretkey"
 };
