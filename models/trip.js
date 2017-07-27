2498eb0a40698d627464c25cb7fd968fe359f104
Unknown <maduraherath8@gmail.com>
2017-07-28 04:16:25 +0530

    change 5

---
+++ models/trip.js
@@ -0,0 +1,29 @@
+const  mongoose = require('mongoose');
+const config = require('../config/database');
+const schema = mongoose.Schema;
+var async = require('async');
+var fs = require('fs');
+var TripSchema = new schema({
+    name:{type:String},
+    waypoints:{type:String},
+    from:{type:String},
+    to : {type:String}
+     
+
+});
+
+
+
+const Triptab =  module.exports = mongoose.model('Triptab',TripSchema);
+
+module.exports.addTrip = function (newTrip,callback) {
+
+     Triptab.create(newTrip,callback);
+
+}
+
+
+module.exports.getTrip = function (callback) {
+     
+    Triptab.find(callback);
+};
--- src/pages/trip-list/trip-list.ts
+++ src/pages/trip-list/trip-list.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Map } from '../../pages/map/map';
+import {TripService} from '../service/trip.service';
 /**
  * Generated class for the TripList page.
  *
@@ -14,11 +15,18 @@ import { Map } from '../../pages/map/map';
 })
 export class TripList {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public tripservice : TripService) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TripList');
+  }
+  trips : Trip;
+  gettrip(){
+    this.tripservice.getTrips()
+    .subscribe(trips =>{
+      this.trips = trips
+    })
   }
 pushPage(){
       this.navCtrl.push(Map, {
